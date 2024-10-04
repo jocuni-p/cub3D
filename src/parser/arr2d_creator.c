4b9bef7fdef1db0b441c9a08ebf5b33b4db0057a
Joan Cuní Palà <jocuni-p@cbr1s4.42barcelona.com>
2024-10-04 19:17:38 +0200

    'arr2d_creator' in progress

--- src/parser/arr2d_creator.c
+++ src/parser/arr2d_creator.c
@@ -6,7 +6,7 @@
 /*   By: jocuni-p <jocuni-p@student.42barcelona.com +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 11:14:18 by jocuni-p          #+#    #+#             */
-/*   Updated: 2024/10/03 18:17:20 by jocuni-p         ###   ########.fr       */
+/*   Updated: 2024/10/04 18:47:29 by jocuni-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int	map_size(t_parser *parser)
 		tmp = tmp->next;
 		high++;
 	}
-	if (len < 3 || high < 3)
+	if (len < 3 || high < 3)//tamanyo minimo de caracteres y lineas que podria presentar el mapa
 		return (handle_error(ERR_MAP), 1);
 	parser->map.w = len;
 	parser->map.h = high;
@@ -45,10 +45,25 @@ int	arr2d_creator(t_parser *parser)
 {
 	if (map_size(parser))
 		return (1);
-//		map_size per fer el malloc de la matriu	
+//MALLOCADO DE LA MATRIZ EXTERNA
+	parser->raw_map = (char **)malloc(sizeof(char *) * parser->map.h + 1);
+	if (!parser->raw_map)
+		return (handle_error(ERR_MAP), 1);//ademas, liberar todo allocado hasta ahora
+//MALLOCADO DE TODOS LOS ELEMENTOS AL MISMO TAMANYO, ULTIMO A NULL>>>>>>>>>>>>>>
+	reservo memoria con malloc(size_t num_elem, sizeof(char)) para map.w (el padding NO hace falta porque inicio y fin de linea estan verificados a 1)
+	funcion que inicializa cada byte del str con " " para que la memoria no contengan basura que me entorpeceria a la hora de chequear si contiene un " " o un '1'
+	relleno cada elemento de la matriz con su linea, donde no alcance el relleno habra un " "
+	entre la 2a y penultima linea, recorro todos los '0' mirando arriba, abajo, der, izq si hay algun ' '->INVALID
+	sustituyo espacios iniciales y finales de cada linea por '1' 
+
+
+
+	while (parser->map_firstline)
+	{
+
+		map_firstline = map_firstline->next;
+	}
 	return (0);
 }
 
-//si la matriz tiene menos de 3 de ancho y menos de 3 de alto (medidas minimas)->INVALID
-//si la matriz contiene player y solo 1 player-> seguir
-//si el mapa esta abierto (es decir, un 0 junto a un espacio)->INVALID
+//si la matriz contiene player_qty != 1 -> INVALID
